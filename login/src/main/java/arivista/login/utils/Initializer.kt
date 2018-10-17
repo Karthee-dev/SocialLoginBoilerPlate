3595875f14f3b8ac0d516e1e2e388d812e59d51c
karthee <karthegeyan.kumaragurubaran@arivistadigital.com>
2018-10-17 14:27:55 +0530

    refactoring

--- login/src/main/java/arivista/login/utils/Initializer.kt
+++
@@ -1,9 +0,0 @@
-package arivista.login.utils
-
-import android.annotation.SuppressLint
-import android.content.Context
-
-@SuppressLint("StaticFieldLeak")
-object Initializer {
-  lateinit var context: Context
-}
